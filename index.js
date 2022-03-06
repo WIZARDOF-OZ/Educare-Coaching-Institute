225b6b2cd3dfef487d6e582a6dc7235a89320133
WIZARD OF OZ <nurajabegum81@gmail.com>
2022-03-06 15:04:03 +0530

    fix faq

--- index.js
+++ index.js
@@ -10,6 +10,18 @@ const faqs = document.querySelectorAll(".faq")
 
 faqs.forEach(faq => {
     faq.addEventListener('click', () => {
-        faq.classList.toggle('open')
-    })
+        faq.classList.toggle('open');
+
+ // change the icon
+
+        const icon = faq.querySelector('.faq_icon i');
+        if(icon.className === 'uil uil-plus'){
+            icon.className = "uil uil-minus"
+        }else{
+            icon.className = "uil uil-plus"
+        }
+    }) 
+
+   
+  
 })
