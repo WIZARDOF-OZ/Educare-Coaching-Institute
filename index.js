7a9c0c4d887380d1c87563b00e18935a8e558cd9
WIZARD OF OZ <nurajabegum81@gmail.com>
2022-03-06 14:45:06 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// change navbar styles on scroll
+
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0)
+})
+
+// show/hide faq answer
+
+const faqs = document.querySelectorAll(".faq")
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open')
+    })
+})
